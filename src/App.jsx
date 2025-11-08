82b603c3804e77034dca60b78ad310b155ff399a
Damn-Nachiket <alphaonfield@gmail.com>
2025-11-09 04:58:57 +1030

    Implementd 3D Product Viewer Section with different scale of laptop

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import Hero from "./Components/Hero.jsx"
 import Navbar from "./Components/navbar.jsx"
 import ProductViewer from "./Components/ProductViewer.jsx"
+import gsap from "gsap"
+import { ScrollTrigger } from "gsap/all"
+
+gsap.registerPlugin(ScrollTrigger)
 
 const App = () => {
     return (
--- src/Components/ProductViewer.jsx
+++ src/Components/ProductViewer.jsx
@@ -3,23 +3,28 @@ import useMacBookStore from "../store"
 import { Canvas } from "@react-three/fiber";
 import { Box, OrbitControls } from "@react-three/drei";
 import MacbookModel14 from "./models/Macbook-14";
-import StudioLight from "./StudioLight";
+import StudioLight from "./three/StudioLight";
+
+import ModelSwitcher from "./three/ModelSwitcher";
+import { useMediaQuery } from "react-responsive";
 
 const ProductViewer = () => {
 
     const {color, scale, setColor, setScale} = useMacBookStore();
+
+    const isMobile = useMediaQuery({ query: '(max-width: 1024px)'});
   return (
     <section id="product-viewer">
         <h2> Take a closer look</h2>
 
         <div className="controls">
-            <p className="info">Macbook Pro {scale} in {color}</p>
+            <p className="info">Macbook Pro | Avaialable in 14" and 16" in Space Grey and Dark Colors</p>
 
             <div className="flex-center gap-5 mt-5">
                 <div className="color-control">
                     <div 
-                        onClick={() => setColor('#adb5db')} 
-                        className={clsx('bg-neutral-300', color === '#adb5db' && 'active')} 
+                        onClick={() => setColor('#D4D6D6')} 
+                        className={clsx('bg-neutral-300', color === '#D4D6D6' && 'active')} 
                     />
                     <div 
                         onClick={() => setColor('#2e2c2e')} 
@@ -44,9 +49,7 @@ const ProductViewer = () => {
         <Canvas id="canvas" camera={{position:[0,2,5], fov:50, near:0.1, far:100}}>
             <StudioLight />
 
-            <MacbookModel14 scale={0.06} position={[0, 0, 0]} />
-
-            <OrbitControls enableZoom={false} />
+            <ModelSwitcher scale={isMobile ? scale - 0.03 : scale} isMobile={isMobile}/>
         </Canvas>
 
     </section>
